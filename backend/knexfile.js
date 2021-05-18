f7699977e2ac8daa8c68af1c5ec834806d6a0ca7
tatsuya98 <nathanbailey_62@hotmail.com>
2021-05-18 01:41:13 +0100

    change

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -21,7 +21,7 @@ module.exports = {
         client:'pg',
         connection: process.env.DATABASE_URL,
         migrations: {
-            directory: './migrations'
+            tableName: 'migrations'
         },
         pool: {
             min: 2,
