5c82ec9b0e348af1562fa737b2d1339b717bafa4
tatsuya98 <nathanbailey_62@hotmail.com>
2021-05-25 20:24:10 +0100

    test

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,9 @@ const db = knex({
 })
 app.use(cors())
 app.use(express.json())
-app.use('/static',express.static(path.join(__dirname,'build')))
+app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+  });
 app.post('/register',(req,res)=>{register.handleRegister(req,res,db,bcrypt)})
 app.post('/login',(req,res)=>{login.handleLogin(req,res,db,bcrypt)})
 app.get('/:user_id',(req,res)=>{expenses.handleExpenses(req,res,db)})
